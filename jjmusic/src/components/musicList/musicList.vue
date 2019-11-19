f69c07d1180cd6dad32d58406ca3ea393643e63f
ShenJieSuzhou <745689122@qq.com>
2019-11-19 17:03:40 +0800

    添加二级样式和组件
    添加二级样式和组件

--- jjmusic/src/components/musicList/musicList.vue
+++ jjmusic/src/components/musicList/musicList.vue
@@ -39,6 +39,14 @@
 </template>
 
 <script>
+import Scroll from '../scroll/scroll'
+import SongList from '../songLists/songList'
+import Loading  from '../loading/loading'
+
+
+
+
+
 export default {
     
 }
@@ -46,6 +54,141 @@ export default {
 
 
 <style lang="scss">
+@import "../../style/scss/mixin.scss";
+@import "../../style/scss/variable.scss";
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.2s
+}
+.slide-enter, .slide-leave-to {
+  transform: translate3d(30%, 0, 0);
+  opacity: 0;
+}
 
+.music-list {
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: $color-background;
+  .header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 44px;
+    color: #fff;
+    z-index: 100;
+    .back {
+      position: absolute;
+      top: 0;
+      left: 4px;
+      .fa-angle-left {
+        padding: 5px 10px;
+        font-size: 30px;
+      }
+    }
+    .text {
+      position: absolute;
+      left: 38px;
+      line-height: 44px;
+      font-size: $font-size-medium-x;
+      @include no-wrap()
+    }
+  }
+  .list {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    background: $color-background;
+    .music-list-wrapper {
+      .bg-image {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding-top: 75%;
+        transform-origin: top;
+        background-size: cover;
+        background-position: 0 30%;
+        .filter {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: black;
+          opacity: 0.2;
+        }
+        .text {
+          position: absolute;
+          width: 80%;
+          height: 40px;
+          bottom: 50px;
+          left: 15px;
+          color: #fff;
+          .play-count {
+            position: absolute;
+            bottom: -16px;
+            font-size: $font-size-small;
+          }
+          .list-title {
+            position: absolute;
+            bottom: 0;
+            font-size: $font-size-medium-x;
+            line-height: 18px;
+            font-weight: bold;
+            letter-spacing: 1px;
+          }
+        }
+      }
+      .song-list-wrapper {
+        padding: 41px 0 20px 0;
+        border-radius: 10px;
+        position: relative;
+        top:-20px;
+        background: $color-background;
+        .sequence-play {
+          position: absolute;
+          // left: 8;
+          top: 0px;
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 40px;
+          padding-left: 16px;
+          border-bottom: 1px solid rgb(228, 228, 228);
+          .iconfont {
+            font-size: 18px;
+            color: $color-text;
+            padding-right: 14px;
+          }
+          .text {
+            font-size: $font-size-medium-x;
+          }
+          .count {
+            font-size: $font-size-medium;
+            color: $color-text-g;
+          }
+        }
+      }
+      // position: fixed;
+      // top: 0;
+      // bottom: 0;
+      // width: 100%;
+      // background: $color-background;
+      // height: 100%;
+      // z-index: 20;
+      // overflow: hidden;
+        // padding: 5px 0 20px 0;
+    }
+  }
+  .loading-content {
+    position: fixed;
+    width: 100%;
+    top: 70%;
+    transform: translateY(-50%);
+  }
+}
 
 </style>
